1522d811dedf9e5c8a6b229ac98d00f26ab387c3
R Greene <greenbert81@gmail.com>
2022-10-09 00:35:17 -0700

    Env variables for MongoDB

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,7 +41,7 @@ const typeDefs = gql`
 	type Mutation {
         createUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!, username: String): Auth
-        saveBook(book: bookInput): User
+        saveBook(book: BookInput): User
         removeBook(bookId: ID!): User	
 	}
 `;
